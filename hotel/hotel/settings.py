4531c3fbdd5ed380b1325a5fd8703c73ad2f5a0e
akhilmohan123 <akhilmohan8547@gmail.com>
2024-08-10 13:46:18 +0530

    created user and profile custom model

--- hotel/hotel/settings.py
+++ hotel/hotel/settings.py
@@ -137,6 +137,7 @@ MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+AUTH_USER_MODEL="userauths.User"
 JAZZMIN_SETTINGS = {
     "site_title": "your_site_name",
     "site_header": "your_site_header",
--- hotel/userauths/admin.py
+++ hotel/userauths/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from userauths.models import User,Profile
 # Register your models here.
+class Useradmin(admin.ModelAdmin):
+    search_fields=['full_name','username']
+    list_display=['username','full_name','email','phone','gender']
+class Profileadmin(admin.ModelAdmin):
+    search_fields=['full_name','user_username']
+    list_display=['full_name','verified','user']
+admin.site.register(User,Useradmin)
+admin.site.register(Profile,Profileadmin)
